33aa488e344e527d6df1473f32fb131dc27b7f99
Phrank <ephranky@gmail.com>
2023-08-13 21:02:07 +0100

    my country div

--- js/theme.js
+++ js/theme.js
@@ -1,13 +1,10 @@
 const icon = document.querySelector(".bx");
 const toggle = document.querySelector(".toggle");
-const backBtn = document.querySelector(".back-btn");
-const btn = document.getElementsByTagName("btn");
-console.log(btn);
+let btn = document.getElementsByTagName("a");
 
 toggle.addEventListener("click", (e) => {
   document.body.classList.toggle("dark-mode");
   toggle.classList.toggle("dark-mode");
   icon.classList.toggle("bxs-moon");
-  backBtn.style.color = "white";
   btn.style.color = "white";
 });
